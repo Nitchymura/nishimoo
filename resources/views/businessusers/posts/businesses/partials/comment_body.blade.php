13a9e6159b6e3a361d7cba15cf901d3d46d7afd2
Takeshi <nitchy_mura_tkc@yahoo.co.jp>
2025-08-09 21:22:11 +0900

    business-comment

--- resources/views/businessusers/posts/businesses/partials/comment_body.blade.php
+++ resources/views/businessusers/posts/businesses/partials/comment_body.blade.php
@@ -151,5 +151,5 @@
         @endforelse         
     </div>
 </div>
-        @vite('resources/js/quest/comment/quest-comment.js')
+        {{-- @vite('resources/js/quest/comment/quest-comment.js') --}}
 
